0aaefaa8d8ae45bd8ec3a40995b3a47eb862542b
aymenmzz <105977772+aymenmzz@users.noreply.github.com>
2022-08-02 09:33:57 -0700

    Add files via upload

--- pages/cards/[domains]/index.js
+++ pages/cards/[domains]/index.js
@@ -134,6 +134,7 @@ export default function RenderDomains({ flashCards, removeFlashCard }) {
   );
 }
   catch(e) {
+    console.log(JSON.parse(localStorage.getItem("FlashCards")))
     console.log(flashCards)
     console.log(e)
     return <></>
