1d0c16531983e5e4875e5ef39385a52490129e56
aymenmzz <105977772+aymenmzz@users.noreply.github.com>
2022-09-13 23:56:20 -0700

    Delete CardList.js

--- components/CardList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
-
-export default function CardList({ flashCards, removeFlashCard }) {
-  return <></>;
-}
